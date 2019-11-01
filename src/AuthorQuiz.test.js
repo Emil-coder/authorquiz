876087904b0f0611ead499fca3c36bfa441279c7
Emil Voltaire <emil.voltaire@54u.se>
2019-11-01 17:10:15 +0100

    added test for onAnswerSelected

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -66,6 +66,23 @@ describe("Author Quiz", () => {
 
   });
 
+  describe("When the first answer is selected", () => {
+    let wrapper;
+    // jest.fn() creates a mock function
+    const handleAnswerSelected = jest.fn();
+
+    beforeAll(() => {
+      wrapper = mount(
+        <AuthorQuiz {...state} onAnswerSelected={handleAnswerSelected} />);
+      wrapper.find('.answer').first().simulate('click');
+    });
+
+    it("onAnswerSelected should be called", () => {
+      expect(handleAnswerSelected).toHaveBeenCalled();
+    });
+
+  });
+
 });
 
 
