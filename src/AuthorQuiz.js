76fd8cd8e46b5300c97d6b3014b36c4a4dd17a8e
Emil Voltaire <emil.voltaire@54u.se>
2019-11-06 11:57:28 +0100

    Continue button working

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -57,7 +57,7 @@ function Continue({ show, onContinue }) {
     <div className="row continue">
       {show
         ? <div className="col-11">
-          <button className="btn btn-primary btn-lg float-right">Continue</button>
+          <button className="btn btn-primary btn-lg float-right" onClick={onContinue}>Continue</button>
         </div>
         : null}
     </div>
